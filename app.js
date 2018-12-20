94e9ec68eb2a944306e0c9beabc339007055c77c
Govardhan Reddy <bobbiligovardhanreddy@gmail.com>
2018-12-20 12:22:56 +0530

    Auth  handling

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+//Load User Model
+require('./models/User');
+
 // Passport config
 require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
+
+// Load keys
+const keys = require('./config/keys');
+
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI(), {
+    useNewUrlParser: true
+}).then(()=>{
+    console.log("Mongo Connected");
+}).catch(err => console.log(err));
 
 const app = express();
 
@@ -13,6 +30,23 @@ const app = express();
 app.get('/', (req,res)=>{
     res.send('it works');
 });
+
+app.use(cookieParser());
+app.use(session({
+    secret : 'secret',
+    resave : false,
+    saveUninitialized : false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set Gloabl vars
+app.use((req,res,next) => {
+    res.locals.user = req.user || null,
+    next();
+})
 
 // Use Routes
 app.use('/auth',auth);
@@ -22,3 +56,9 @@ const port = process.env.PORT || 4002;
 app.listen(port, ()=>{
     console.log(`server started on port ${port}`);
 });
+
+
+
+
+
+
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,13 @@
 module.exports = {
-    mongoURI : 'mongodb://govardhan:weyvtest1@ds139534.mlab.com:39534/storybooks-dev',
+    mongoURI : function () {
+        if(process.env.NODE_ENV === 'production'){
+            return 'mongodb://govardhan:weyvtest1@ds139534.mlab.com:39534/storybooks-dev'
+        }
+        else{
+            return 'mongodb://localhost:27017/storybooks-dev'
+        }
+
+    },
     googleClientID : '860023107603-5sh93l52vj6e473lqoj2ph91n2jg275t.apps.googleusercontent.com',
     googleClientSecret : 'qdIZP7Mfz50wjIeteKPF8zBt'
 }
